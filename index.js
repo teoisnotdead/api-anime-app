4496710cda717f4463e6304f982a0b3009fc5cbd
TEO <alfredo.ssm@gmail.com>
2023-01-20 17:11:06 -0300

    Changes for deploy

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const cookie = require('cookie-parser');
+const anime = require('./anime.controller');
+const app = express();
+const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://730teo:teo730@animeapp.b2odxts.mongodb.net/animeapp?retryWrites=true&w=majority')
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+};
 
-module.exports = mongoose;
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookie());
+app.get("/", (req, res) => {
+    res.send(`<h1
+    style="display: flex; justify-content: center; align-items: center; width: 100%; min-height: 100vh; margin: 0; padding: 0; font-size: 7rem;">
+    App Anime Server</h1>`);
+});
+
+app.get("/anime/list", anime.list);
+app.post("/anime/add", anime.create);
+app.put("/anime/update/:animeId", anime.update);
+app.delete("/anime/delete/:animeId", anime.delete);
+app.get('*', (req, res) => {
+    res.status(404).send(`<h1
+        style="display: flex; justify-content: center; align-items: center; width: 100%; min-height: 100vh; margin: 0; padding: 0; font-size: 7rem;">
+        404 Not Found</h1>`);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
